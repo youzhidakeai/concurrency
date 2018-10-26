0795b0d664574d7c615195a676c105df0b0fb09c
youzhidakeai <wochiyouzhi@gmail.com>
2018-10-27 00:03:12 +0800

    add annoations

---
+++ src/main/java/com/mmall/concurrency/annoations/Recommend.java
@@ -0,0 +1,25 @@
+package com.mmall.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: kkc
+ * Email: wochiyouzhi@gmail.com
+ * Date: 2018-10-26
+ * Time: 下午11:57
+ */
+
+/**
+ * 课程里用来标记【推荐】的类或者写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Recommend {
+    String value() default "";
+
+}
